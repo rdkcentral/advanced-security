116f39e5748fac1030e742d9775b57a81d88ec54
sowmiyachelliah <sowmiya_chelliah@comcast.com>
2026-04-14 09:36:50 +0000

    Updating cosa_adv_security_internal.c

--- source/AdvSecurityDml/cosa_adv_security_internal.c
+++ source/AdvSecurityDml/cosa_adv_security_internal.c
@@ -99,6 +99,7 @@
 #define ADVSEC_AGENT_LOG_MAX_SIZE (2 * 1024 * 1024)  /* 2MB */
 #define ADVSEC_AGENT_LOG_INTERVAL 5.0
 #define LOGROTATE_BINARY "/usr/sbin/logrotate"
+#define ADVSEC_AGENT_LOGROTATE_CONF "/etc/logrotate.d/advsec-agent"
 
 #ifdef CONFIG_CISCO
 #define CONFIG_VENDOR_NAME  "Cisco"
