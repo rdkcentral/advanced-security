74c67c0af96711e789c2b83a0c1f9727a25dfc36
sowmiyachelliah <sowmiya_chelliah@comcast.com>
2026-04-29 05:58:38 +0000

    Enable CUJO-NI log rotation only when RFC is enabled

--- source/AdvSecurityDml/cosa_adv_security_internal.c
+++ source/AdvSecurityDml/cosa_adv_security_internal.c
@@ -96,12 +96,15 @@
 
 /* Logrotate configuration for agent.txt and cujo-ni.txt */
 #define ADVSEC_AGENT_LOG_FILE "/rdklogs/logs/agent.txt"
-#define ADVSEC_NI_LOG_FILE "/rdklogs/logs/cujo-ni.txt"
 #define ADVSEC_AGENT_LOG_MAX_SIZE (2 * 1024 * 1024)  /* 2MB */
 #define ADVSEC_LOG_MONITOR_INTERVAL 5.0  /* Check every 5 seconds */
 #define LOGROTATE_BINARY "/usr/sbin/logrotate"
 #define ADVSEC_AGENT_LOGROTATE_CONF "/etc/logrotate.d/advsec-agent"
+
+#ifdef NETWORK_INTELLIGENCE
+#define ADVSEC_NI_LOG_FILE "/rdklogs/logs/cujo-ni.txt"
 #define ADVSEC_NI_LOGROTATE_CONF "/etc/logrotate.d/advsec-ni"
+#endif
 
 #ifdef CONFIG_CISCO
 #define CONFIG_VENDOR_NAME  "Cisco"
@@ -1911,11 +1914,13 @@ void rotate_agent_log(void)
     rotate_log_file(ADVSEC_AGENT_LOG_FILE, "Advsec Agent", ADVSEC_AGENT_LOGROTATE_CONF);
 }
 
+#ifdef NETWORK_INTELLIGENCE
 /* Log rotation function for cujo-ni.txt */
 void rotate_ni_log(void)
 {
     rotate_log_file(ADVSEC_NI_LOG_FILE, "Network Intelligence", ADVSEC_NI_LOGROTATE_CONF);
 }
+#endif
 
 /* Callback function for agent.txt libev stat watcher */
 void agent_log_stat_cb(EV_P_ ev_stat *w, int revents)
@@ -1929,6 +1934,7 @@ void agent_log_stat_cb(EV_P_ ev_stat *w, int revents)
     }
 }
 
+#ifdef NETWORK_INTELLIGENCE
 /* Callback function for cujo-ni.txt libev stat watcher */
 void ni_log_stat_cb(EV_P_ ev_stat *w, int revents)
 {
@@ -1940,6 +1946,7 @@ void ni_log_stat_cb(EV_P_ ev_stat *w, int revents)
         rotate_ni_log();
     }
 }
+#endif
 
 /* Thread function to run libev event loop for log monitoring */
 void* agent_log_monitor_thread(void* arg)
@@ -1948,7 +1955,9 @@ void* agent_log_monitor_thread(void* arg)
 
     struct ev_loop *loop = NULL;
     static ev_stat agent_stat_watcher;
+#ifdef NETWORK_INTELLIGENCE
     static ev_stat ni_stat_watcher;
+#endif
 
     CcspTraceDebug(("Starting agent log monitor thread\n"));
 
@@ -1964,10 +1973,20 @@ void* agent_log_monitor_thread(void* arg)
     ev_stat_start(loop, &agent_stat_watcher);
     CcspTraceDebug(("Advsec Agent log monitoring started on %s\n", ADVSEC_AGENT_LOG_FILE));
 
-    /* Monitor cujo-ni.txt */
-    ev_stat_init(&ni_stat_watcher, ni_log_stat_cb, ADVSEC_NI_LOG_FILE, ADVSEC_LOG_MONITOR_INTERVAL);
-    ev_stat_start(loop, &ni_stat_watcher);
-    CcspTraceDebug(("Network Intelligence log monitoring started on %s\n", ADVSEC_NI_LOG_FILE));
+#ifdef NETWORK_INTELLIGENCE
+    /* Monitor cujo-ni.txt only if NI RFC is enabled */
+    if (g_pAdvSecAgent && g_pAdvSecAgent->pAdvNetworkIntelligence_RFC && 
+        g_pAdvSecAgent->pAdvNetworkIntelligence_RFC->bEnable)
+    {
+        ev_stat_init(&ni_stat_watcher, ni_log_stat_cb, ADVSEC_NI_LOG_FILE, ADVSEC_LOG_MONITOR_INTERVAL);
+        ev_stat_start(loop, &ni_stat_watcher);
+        CcspTraceDebug(("Network Intelligence log monitoring started on %s\n", ADVSEC_NI_LOG_FILE));
+    }
+    else
+    {
+        CcspTraceInfo(("Network Intelligence RFC disabled, skipping NI log monitoring\n"));
+    }
+#endif
 
     ev_run(loop, 0);
     ev_loop_destroy(loop);
